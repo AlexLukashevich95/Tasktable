70871998dc1d419400c491b8701e3628758a3763
Alex Lukashevich <alex.lukashevich95@gmail.com>
2024-08-26 22:09:00 +0300

    Feature-task2: -delete comma in sql script

--- database/ddl/create_tasktable.sql
+++ database/ddl/create_tasktable.sql
@@ -4,5 +4,5 @@ CREATE TABLE tasks (
     name VARCHAR(255) NOT NULL,      -- Название задачи
     description TEXT,                -- Описание задачи
     deadline DATE,                   -- Крайний срок выполнения задачи
-    status VARCHAR(50) NOT NULL,     -- Статус задачи (например, 'Pending', 'In Progress', 'Completed')
+    status VARCHAR(50) NOT NULL     -- Статус задачи (например, 'Pending', 'In Progress', 'Completed')
 );
